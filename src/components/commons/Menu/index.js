fac3b1f7af3ec041c91beee1e502cd343c1d89c6
IrleySilvestre <66978110+IrleySilvestre@users.noreply.github.com>
2021-02-11 00:15:32 -0300

    ir dormir

--- src/components/commons/Menu/index.js
+++ src/components/commons/Menu/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Logo } from '../../../theme/logo/Logo'
-import { MenuWrapper, ButtonCadastrar, ButtonEntrar } from './styles/MenuWrapper.js'
+import { MenuWrapper, ButtonCadastrar, ButtonEntrar } from '../menu/styles/MenuWrapper.js'
 
 const links = [
     {
