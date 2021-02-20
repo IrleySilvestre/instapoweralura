8759a57db0cc4e582da7a7c260a4a7736164728b
IrleySilvestre <66978110+IrleySilvestre@users.noreply.github.com>
2021-02-20 18:10:57 -0300

    Ajustes de formatacao Grid

--- src/components/foundation/layout/Grid/index.js
+++ src/components/foundation/layout/Grid/index.js
@@ -12,41 +12,42 @@ const Col = styled.div`
             return css`
                 flex: 0 0 ${(100 * value) / 12}%;
                 max-width: ${(100 * value) / 12}%;
-    `;
+             `;
         }
         return breakpointsMedia({
             ...(value.xs && {
                 xs: css`
                 flex: 0 0 ${(100 * value.xs) / 12}%;
                 max-width: ${(100 * value.xs) / 12}%;
-    `,
+            `,
             }),
             ...(value.sm && {
                 sm: css`
                 flex: 0 0 ${(100 * value.sm) / 12}%;
                 max-width: ${(100 * value.sm) / 12}%;
-    `,
+            `,
             }),
             ...(value.md && {
                 md: css`
                 flex: 0 0 ${(100 * value.md) / 12}%;
                 max-width: ${(100 * value.md) / 12}%;
-    `,
+            `,
             }),
             ...(value.lg && {
                 lg: css`
-      flex: 0 0 ${(100 * value.lg) / 12}%;
-      max-width: ${(100 * value.lg) / 12}%;
-    `,
+                flex: 0 0 ${(100 * value.lg) / 12}%;
+                max-width: ${(100 * value.lg) / 12}%;
+            `,
             }),
             ...(value.xl && {
                 xl: css`
                 flex: 0 0 ${(100 * value.xl) / 12}%;
                  max-width: ${(100 * value.xl) / 12}%;
-    `,
+              `,
             }),
         });
     }}
+  
   ${({ offset }) => {
         if (typeof offset === 'number') {
             return css`
@@ -90,39 +91,41 @@ Col.defaultProps = {
 
 export const Grid = {
     Container: styled.div`
-    width: 100%;
-    padding-right: 28px;
-    padding-left: 28px;
-    margin-right: auto;
-    margin-left: auto;
-    ${breakpointsMedia({
-        xs: css`
-      max-width: initial;
- 
+      width: 100%;
       padding-right: 28px;
       padding-left: 28px;
+      margin-right: auto;
+      margin-left: auto;
+      ${breakpointsMedia({
+            xs: css`
+            max-width: initial;
+    
+            padding-right: 28px;
+            padding-left: 28px;
+          `,
+            sm: css`
+            max-width: 576px; 
+          `,
+            md: css`
+            max-width: 768px;
+            padding-right: 16px;
+            padding-left: 16px; 
+         `,
+            lg: css`
+            max-width: 1160px; 
+          `,
+            xl: css`
+            max-width: 1222px;
+          `,
+        })}
     `,
-        sm: css`
-      max-width: 576px; 
-    `,
-        md: css`
-      max-width: 768px;
-      padding-right: 16px;
-      padding-left: 16px; 
-    `,
-        lg: css`
-      max-width: 1160px; 
-    `,
-        xl: css`
-      max-width: 1222px;
-    `,
-    })}
-  `,
+
     Row: styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    margin-right: -16px;
-    margin-left: -16px;
-  `,
+      display: flex;
+      flex-wrap: wrap;
+      margin-right: -16px;
+      margin-left: -16px;
+    `,
+    
     Col,
 }; 
